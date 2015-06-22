0f717ccf1bb1ae50df8eb6c0600a7f9b709f612d
Stella Lie <ask@stellalie.com>
2015-06-22 15:59:36 +1000

    Finish up unit test for inline choice interaction

--- src/Mappers/QtiV2/Import/Interactions/InlineChoiceInteraction.php
+++ src/Mappers/QtiV2/Import/Interactions/InlineChoiceInteraction.php
@@ -10,10 +10,12 @@ use Learnosity\Entities\QuestionTypes\clozedropdown_validation_valid_response;
 use Learnosity\Exceptions\MappingException;
 use Learnosity\Mappers\QtiV2\Import\ResponseProcessingTemplate;
 use Learnosity\Mappers\QtiV2\Import\Utils\QtiComponentUtil;
+use qtism\data\state\MapEntry;
 
 class InlineChoiceInteraction extends AbstractInteraction
 {
     private $choicesMapping = [];
+    private $caseSensitive = false;
 
     public function getQuestionType()
     {
@@ -31,6 +33,7 @@ class InlineChoiceInteraction extends AbstractInteraction
         if ($validation instanceof clozedropdown_validation) {
             $question->set_validation($validation);
         }
+        $question->set_case_sensitive($this->caseSensitive);
         return $question;
     }
 
@@ -51,40 +54,77 @@ class InlineChoiceInteraction extends AbstractInteraction
             return null;
         }
         if ($this->responseProcessingTemplate->getTemplate() === ResponseProcessingTemplate::MATCH_CORRECT) {
-            $correctResponse = $this->responseDeclaration->getCorrectResponse();
-            if (!empty($correctResponse->getInterpretation())) {
-                // TODO: should warn that this is ignored
-            }
+            return $this->buildMatchCorrectValidation();
+        } if ($this->responseProcessingTemplate->getTemplate() === ResponseProcessingTemplate::MAP_RESPONSE) {
+            return $this->buildMapResponseTemplate();
+        } else {
+            $this->exceptions[] = new MappingException('Does not support template ' . $this->responseProcessingTemplate->getTemplate() .
+                ' on <responseProcessing>. Ignoring <responseProcessing>');
+            return null;
+        }
+    }
 
-            $validResponseValues = [];
-            foreach ($correctResponse->getValues() as $key => $value) {
-                $validResponseValues[] = (string)$this->choicesMapping[$value->getValue()];
-            }
+    private function buildMatchCorrectValidation()
+    {
+        $validResponseValues = [];
+        foreach ($this->responseDeclaration->getCorrectResponse()->getValues() as $key => $value) {
+            $validResponseValues[] = (string) $this->choicesMapping[$value->getValue()];
+        }
 
-            $validation = new clozedropdown_validation();
-            $validation->set_scoring_type('exactMatch');
+        $validation = new clozedropdown_validation();
+        $validation->set_scoring_type('exactMatch');
+
+        // First response pair shall be mapped to `valid_response`
+        $firstValidResponseValue = array_shift($validResponseValues);
+        $validResponse = new clozedropdown_validation_valid_response();
+        $validResponse->set_score(1);
+        $validResponse->set_value([$firstValidResponseValue]);
+        $validation->set_valid_response($validResponse);
+
+        // Others go in `alt_responses`
+        $altResponses = [];
+        foreach ($validResponseValues as $otherResponseValues) {
+            $item = new clozedropdown_validation_alt_responses_item();
+            $item->set_score(1);
+            $item->set_value([$otherResponseValues]);
+            $altResponses[] = $item;
+        }
+        $validation->set_alt_responses($altResponses);
+        return $validation;
+    }
+
+    private function buildMapResponseTemplate()
+    {
+        $validation = new clozedropdown_validation();
+        $validation->set_scoring_type('exactMatch');
+
+        $altResponses = [];
+        foreach ($this->responseDeclaration->getMapping()->getMapEntries()->getArrayCopy(true) as $key => $mapEntry) {
+            /** @var MapEntry $mapEntry */
+
+            // If one of them case sensitive, the whole things then be it
+            if ($mapEntry->isCaseSensitive()) {
+                $this->caseSensitive = true;
+            }
 
             // First response pair shall be mapped to `valid_response`
-            $firstValidResponseValue = array_shift($validResponseValues);
-            $validResponse = new clozedropdown_validation_valid_response();
-            $validResponse->set_score(1);
-            $validResponse->set_value([$firstValidResponseValue]);
-            $validation->set_valid_response($validResponse);
-
-            // Others go in `alt_responses`
-            $altResponses = [];
-            foreach ($validResponseValues as $otherResponseValues) {
-                $item = new clozedropdown_validation_alt_responses_item();
-                $item->set_score(1);
-                $item->set_value([$otherResponseValues]);
-                $altResponses[] = $item;
+            if ($key === 0) {
+                $validResponse = new clozedropdown_validation_valid_response();
+                $validResponse->set_value([$this->choicesMapping[$mapEntry->getMapKey()]]);
+                $validResponse->set_score($mapEntry->getMappedValue());
+                $validation->set_valid_response($validResponse);
+            } else {
+                // Others go in `alt_responses`
+                $altResponseItem = new clozedropdown_validation_alt_responses_item();
+                $altResponseItem->set_value([$this->choicesMapping[$mapEntry->getMapKey()]]);
+                $altResponseItem->set_score($mapEntry->getMappedValue());
+                $altResponses[] = $altResponseItem;
             }
-            $validation->set_alt_responses($altResponses);
-
-            return $validation;
-        } else {
-            throw new MappingException('Does not support template ' . $this->responseProcessingTemplate->getTemplate() .
-                ' on <responseProcessing>', MappingException::CRITICAL);
         }
+
+        if (!empty($altResponses)) {
+            $validation->set_alt_responses($altResponses);
+        }
+        return $validation;
     }
 }
--- src/Tests/Unit/Mappers/QtiV2/Import/Interactions/InlineChoiceInteractionTest.php
+++ src/Tests/Unit/Mappers/QtiV2/Import/Interactions/InlineChoiceInteractionTest.php
@@ -16,6 +16,8 @@ class InlineChoiceInteractionTest extends AbstractInteractionTest
             'melbourne' => 'Melbourne',
             'canberra' => 'Canberra',
         ]);
+        $interaction->mustShuffle(true);
+        $interaction->setRequired(true);
         $mapper = new InlineChoiceInteraction($interaction);
         $question = $mapper->getQuestionType();
 
@@ -24,6 +26,9 @@ class InlineChoiceInteractionTest extends AbstractInteractionTest
         $this->assertEquals('clozedropdown', $question->get_type());
         $validation = $question->get_validation();
         $this->assertNull($validation);
+
+        // By default case sensitive should be false
+        $this->assertFalse($question->get_case_sensitive());
     }
 
     public function testSimpleCaseWithMatchCorrectValidation()
@@ -43,6 +48,14 @@ class InlineChoiceInteractionTest extends AbstractInteractionTest
         $this->assertNotNull($question);
         $this->assertEquals('clozedropdown', $question->get_type());
 
+        // Should populate possible responses
+        $this->assertCount(1, $question->get_possible_responses());
+        $this->assertCount(3, $question->get_possible_responses()[0]);
+        $this->assertContains('Sydney', $question->get_possible_responses()[0]);
+        $this->assertContains('Melbourne', $question->get_possible_responses()[0]);
+        $this->assertContains('Canberra', $question->get_possible_responses()[0]);
+
+        // Should have validation object
         $validation = $question->get_validation();
         $this->assertNotNull($validation);
         $this->assertInstanceOf('Learnosity\Entities\QuestionTypes\clozedropdown_validation', $validation);
@@ -59,10 +72,56 @@ class InlineChoiceInteractionTest extends AbstractInteractionTest
         $this->assertCount(1, $altResponses);
         $this->assertEquals(1, $altResponses[0]->get_score());
         $this->assertEquals(["Melbourne"], $altResponses[0]->get_value());
+
+        // By default case sensitive should be false for `match_correct`
+        $this->assertFalse($question->get_case_sensitive());
     }
 
     public function testSimpleCaseWithMapResponseValidation()
     {
-        //TODO: Do it!
+        $interaction = InlineChoiceInteractionBuilder::buildSimple('testIdentifier', [
+            'sydney' => 'Sydney',
+            'melbourne' => 'Melbourne',
+            'canberra' => 'Canberra',
+        ]);
+        $responseDeclaration = ResponseDeclarationBuilder::buildWithMapping('testIdentifier', [
+            'sydney' => ['0.5', false],
+            'melbourne' => ['1', true]
+        ]);
+
+        $mapper = new InlineChoiceInteraction($interaction, $responseDeclaration, ResponseProcessingTemplate::mapResponse());
+        $question = $mapper->getQuestionType();
+
+        $this->assertNotNull($question);
+        $this->assertEquals('clozedropdown', $question->get_type());
+
+
+        // Should populate possible responses
+        $this->assertCount(1, $question->get_possible_responses());
+        $this->assertCount(3, $question->get_possible_responses()[0]);
+        $this->assertContains('Sydney', $question->get_possible_responses()[0]);
+        $this->assertContains('Melbourne', $question->get_possible_responses()[0]);
+        $this->assertContains('Canberra', $question->get_possible_responses()[0]);
+
+        // Should have validation object
+        $validation = $question->get_validation();
+        $this->assertNotNull($validation);
+        $this->assertInstanceOf('Learnosity\Entities\QuestionTypes\clozedropdown_validation', $validation);
+        $this->assertEquals('exactMatch', $validation->get_scoring_type());
+
+        // Should set both `valid_response` and `alt_responses` for multiple correct values
+        $validResponse = $validation->get_valid_response();
+        $this->assertNotNull($validResponse);
+        $this->assertEquals(0.5, $validResponse->get_score());
+        $this->assertEquals(["Sydney"], $validResponse->get_value());
+
+        $altResponses = $validation->get_alt_responses();
+        $this->assertNotNull($altResponses);
+        $this->assertCount(1, $altResponses);
+        $this->assertEquals(1, $altResponses[0]->get_score());
+        $this->assertEquals(["Melbourne"], $altResponses[0]->get_value());
+
+        // Since one of them case sensitive, so the question would be case sensitive
+        $this->assertTrue($question->get_case_sensitive());
     }
 }
