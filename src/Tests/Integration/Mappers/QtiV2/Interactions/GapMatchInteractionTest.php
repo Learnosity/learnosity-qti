8b2409dcd37315d7f14836e31a513a80a29b93ae
Stella Lie <ask@stellalie.com>
2015-07-01 09:37:33 +1000

    Gap Match WIP, validation match correct answer done, lacking unit tests and map response validation

--- src/Tests/Integration/Mappers/QtiV2/Interactions/GapMatchInteractionTest.php
+++ src/Tests/Integration/Mappers/QtiV2/Interactions/GapMatchInteractionTest.php
@@ -3,6 +3,7 @@
 namespace Learnosity\Tests\Integration\Mappers\QtiV2\Interactions;
 
 use Learnosity\AppContainer;
+use Learnosity\Entities\QuestionTypes\clozeassociation;
 use Learnosity\Utils\FileSystemUtil;
 
 class GapMatchInteractionTest extends \PHPUnit_Framework_TestCase
@@ -19,5 +20,17 @@ class GapMatchInteractionTest extends \PHPUnit_Framework_TestCase
 
         $this->assertNotNull($item);
         $this->assertInstanceOf('Learnosity\Entities\Item\item', $item);
+        $this->assertEquals('gapMatch', $item->get_reference());
+
+        $this->assertCount(1, $questions);
+        $this->assertEquals('gapMatch_RESPONSE', $questions[0]->get_reference());
+        /** @var clozeassociation $question */
+        $question = $questions[0]->get_data();
+
+        $validation = $question->get_validation();
+        $this->assertNotNull($validation);
+        $this->assertEquals('exactMatch', $validation->get_scoring_type());
+        $this->assertEquals(1, $validation->get_valid_response()->get_score());
+        $this->assertEquals(['winter', 'summer'], $validation->get_valid_response()->get_value());
     }
 }
