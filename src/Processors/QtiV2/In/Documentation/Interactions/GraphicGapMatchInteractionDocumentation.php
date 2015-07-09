9a841c3387b950d9ccf3572d4b41437ca245eb96
Frank An <frank.an@learnosity.com>
2015-07-09 12:18:14 +1000

    Adding docs for graphicGapMatchInteraction

---
+++ src/Processors/QtiV2/In/Documentation/Interactions/GraphicGapMatchInteractionDocumentation.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Learnosity\Processors\QtiV2\In\Documentation\Interactions;
+
+use Learnosity\Processors\QtiV2\In\Documentation\InteractionDocumentationInterface;
+use Learnosity\Processors\QtiV2\In\Documentation\QtiDoc;
+
+class GraphicGapMatchInteractionDocumentation implements InteractionDocumentationInterface
+{
+
+    public static function getInteractionDocumentation()
+    {
+        $documentation['@attributes'] = QtiDoc::defaultCommonInteractionAttributeRow();
+        $documentation = array_replace_recursive($documentation, [
+            '@notes' => "
+                The element 'graphicGapMatchInteraction' map to our 'imageclozeassociation' question.
+                Read the documentation: <a href='http://docs.learnosity.com/assessment/questions/questiontypes#imageclozeassociation'>
+                http://docs.learnosity.com/assessment/questions/questiontypes#imageclozeassociation</a>
+                <br /><br />
+                Currently support `exactMatch` and `map_response` validation.
+            ",
+            '@attributes' => [
+                'responseIdentifier' => QtiDoc::support(),
+            ],
+            'prompt' => QtiDoc::support('We map this to our question `stimulus`.'),
+            'object' => QtiDoc::support('We map this to the background image. MIME type should be set correctly. Say "image/png"'),
+            'gapImg' => [
+                '@attributes' => [
+                    'id' => QtiDoc::none(),
+                    'class' => QtiDoc::none(),
+                    'xmllang' => QtiDoc::none(),
+                    'label' => QtiDoc::none(),
+                    'identifier' => QtiDoc::support(),
+                    'fixed' => QtiDoc::none(),
+                    'templateIdentifier' => QtiDoc::none(),
+                    'showHide' => QtiDoc::none(),
+                    'matchGroup' => QtiDoc::none(),
+                    'matchMax' => QtiDoc::none('This is ignored, `clozeassociation` can only match to a maximum of 0 response'),
+                    'matchMin' => QtiDoc::none('This is ignored, `clozeassociation` allow zero responses'),
+                    'objectLabel' => QtiDoc::none()
+                ],
+                'object' => QtiDoc::support()
+            ],
+            'associableHotspot' => [
+                '@attributes' => [
+                    'identifier' => QtiDoc::support(),
+                    'fixed' => QtiDoc::none(),
+                    'templateIdentifier' => QtiDoc::none(),
+                    'showHide' => QtiDoc::none(),
+                    'matchGroup' => QtiDoc::none(),
+                    'shape' => QtiDoc::partial('Rectangle shape support only'),
+                    'coords' => QtiDoc::partial('See attribute shape'),
+                    'hotspotLabel' => QtiDoc::none(),
+                    'matchMax' => QtiDoc::none('We calculate this value based on responseDeclaration data'),
+                    'matchMin' => QtiDoc::none('We calculate this value based on responseDeclaration data'),
+                ],
+            ]
+        ]);
+        $documentation = array_merge($documentation, QtiDoc::defaultBlockStaticRow());
+        return $documentation;
+    }
+}
