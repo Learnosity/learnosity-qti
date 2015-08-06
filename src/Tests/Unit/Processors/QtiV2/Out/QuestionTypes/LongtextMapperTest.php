eeae9df2de6323b902e58465a674e8f91d522b06
Stella Lie <ask@stellalie.com>
2015-08-06 10:48:53 +1000

    Tests files created

---
+++ src/Tests/Unit/Processors/QtiV2/Out/QuestionTypes/LongtextMapperTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Unit\Processors\QtiV2\Out\QuestionTypes;
+
+class LongtextMapperTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ src/Tests/Unit/Processors/QtiV2/Out/QuestionTypes/PlaintextMapperTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Unit\Processors\QtiV2\Out\QuestionTypes;
+
+class PlaintextMapperTest extends \PHPUnit_Framework_TestCase
+{
+
+}
