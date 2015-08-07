63242bce7b393c6906a28500a4431f2c78d654ec
Stella Lie <ask@stellalie.com>
2015-08-07 10:23:59 +1000

    Wrap index.php with `www`

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $version = explode('?', explode('/', $_SERVER['REQUEST_URI'])[1])[0];
-$route = __DIR__ . '/src/' . $version . '/route.php';
+$route = __DIR__ . '/../src/' . $version . '/route.php';
 
 if (is_readable($route)) {
     require_once $route;
