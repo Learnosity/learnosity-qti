7e1fc7945f41a9b46c4e35b94da464276a1bade1
Stella Lie <ask@stellalie.com>
2016-08-10 17:36:21 +1000

    Add more tests and fix few bugs and update latest qti version

--- src/Utils/SimpleHtmlDom/simple_html_dom.php
+++ src/Utils/SimpleHtmlDom/simple_html_dom.php
@@ -46,27 +46,29 @@ namespace LearnosityQti\Utils\SimpleHtmlDom;
  * All of the Defines for the classes below.
  * @author S.C. Chen <me578022@gmail.com>
  */
-define('HDOM_TYPE_ELEMENT', 1);
-define('HDOM_TYPE_COMMENT', 2);
-define('HDOM_TYPE_TEXT',    3);
-define('HDOM_TYPE_ENDTAG',  4);
-define('HDOM_TYPE_ROOT',    5);
-define('HDOM_TYPE_UNKNOWN', 6);
-define('HDOM_QUOTE_DOUBLE', 0);
-define('HDOM_QUOTE_SINGLE', 1);
-define('HDOM_QUOTE_NO',  3);
-define('HDOM_INFO_BEGIN',   0);
-define('HDOM_INFO_END',  1);
-define('HDOM_INFO_QUOTE',   2);
-define('HDOM_INFO_SPACE',   3);
-define('HDOM_INFO_TEXT',    4);
-define('HDOM_INFO_INNER',   5);
-define('HDOM_INFO_OUTER',   6);
-define('HDOM_INFO_ENDSPACE',7);
-define('DEFAULT_TARGET_CHARSET', 'UTF-8');
-define('DEFAULT_BR_TEXT', "\r\n");
-define('DEFAULT_SPAN_TEXT', " ");
-define('MAX_FILE_SIZE', 600000);
+if (!defined('HDOM_TYPE_ELEMENT')) {
+    define('HDOM_TYPE_ELEMENT', 1);
+    define('HDOM_TYPE_COMMENT', 2);
+    define('HDOM_TYPE_TEXT',    3);
+    define('HDOM_TYPE_ENDTAG',  4);
+    define('HDOM_TYPE_ROOT',    5);
+    define('HDOM_TYPE_UNKNOWN', 6);
+    define('HDOM_QUOTE_DOUBLE', 0);
+    define('HDOM_QUOTE_SINGLE', 1);
+    define('HDOM_QUOTE_NO',  3);
+    define('HDOM_INFO_BEGIN',   0);
+    define('HDOM_INFO_END',  1);
+    define('HDOM_INFO_QUOTE',   2);
+    define('HDOM_INFO_SPACE',   3);
+    define('HDOM_INFO_TEXT',    4);
+    define('HDOM_INFO_INNER',   5);
+    define('HDOM_INFO_OUTER',   6);
+    define('HDOM_INFO_ENDSPACE',7);
+    define('DEFAULT_TARGET_CHARSET', 'UTF-8');
+    define('DEFAULT_BR_TEXT', "\r\n");
+    define('DEFAULT_SPAN_TEXT', " ");
+    define('MAX_FILE_SIZE', 600000);
+}
 // helper functions
 // -----------------------------------------------------------------------------
 // get html dom from file
---
+++ tests/Integration/Processors/QtiV2/In/TestMapperTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace LearnosityQti\Tests\Integration\Processors\QtiV2;
+
+use LearnosityQti\AppContainer;
+use LearnosityQti\Entities\Activity\activity;
+use LearnosityQti\Tests\AbstractTest;
+
+class TestMapperTest extends AbstractTest
+{
+    public function testParseWithSections()
+    {
+        $xml = $this->getFixtureFileContents('tests/rtest01.xml');
+        $testMapper = AppContainer::getApplicationContainer()->get('qtiv2_test_mapper');
+        list($activity, $manifest) = $testMapper->parse($xml);
+
+        /** @var activity $activity */
+        $this->assertTrue($activity instanceof activity);
+        $this->assertCount(3, $activity->get_data()->get_items());
+    }
+
+    public function testParseWithSectionsTwo()
+    {
+        $xml = $this->getFixtureFileContents('tests/rtest02.xml');
+        $testMapper = AppContainer::getApplicationContainer()->get('qtiv2_test_mapper');
+        list($activity, $manifest) = $testMapper->parse($xml);
+
+        /** @var activity $activity */
+        $this->assertTrue($activity instanceof activity);
+        $this->assertCount(9, $activity->get_data()->get_items());
+    }
+
+    public function testParseWithSectionsThree()
+    {
+        $xml = $this->getFixtureFileContents('tests/rtest03.xml');
+        $testMapper = AppContainer::getApplicationContainer()->get('qtiv2_test_mapper');
+        list($activity, $manifest) = $testMapper->parse($xml);
+
+        /** @var activity $activity */
+        $this->assertTrue($activity instanceof activity);
+        $this->assertCount(9, $activity->get_data()->get_items());
+    }
+}
