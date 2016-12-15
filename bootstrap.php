242da7e03ea1fea809e4bb80f3028c476d8db426
Peter Huynh <peter.huynh@learnosity.com>
2016-12-15 13:44:46 +1100

    [TESTS] Enable phpunit tests with configs in phpunit.xml

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+$vendorDirs = [
+    __DIR__ . '/vendor', // standalone package
+    __DIR__ . '/../..',  // embedded in another package
+];
+
+// locate the autoload
+$loader = null;
+
+foreach ($vendorDirs as $vendorDir) {
+    $file = "{$vendorDir}/autoload.php";
+
+    if (file_exists($file)) {
+        $loader = require_once($file);
+        break;
+    }
+}
