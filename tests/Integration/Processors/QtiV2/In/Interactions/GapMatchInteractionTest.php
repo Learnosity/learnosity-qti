069e79126da4300cf179f2fb4aafef33dd22906d
Stella Lie <ask@stellalie.com>
2017-07-11 21:09:13 +1000

    Wrote quick test for the implementation for tin

--- tests/Integration/Processors/QtiV2/In/Interactions/GapMatchInteractionTest.php
+++ tests/Integration/Processors/QtiV2/In/Interactions/GapMatchInteractionTest.php
@@ -4,6 +4,7 @@ namespace LearnosityQti\Tests\Integration\Mappers\QtiV2\In\Interactions;
 
 use LearnosityQti\AppContainer;
 use LearnosityQti\Entities\QuestionTypes\clozeassociation;
+use LearnosityQti\Entities\QuestionTypes\imageclozeassociation;
 use LearnosityQti\Tests\AbstractTest;
 
 class GapMatchInteractionTest extends AbstractTest
@@ -97,5 +98,74 @@ class GapMatchInteractionTest extends AbstractTest
         $this->assertEquals(['<img src="images/664892_p01_gi01.gif"/>', 'spring'], $altResponses[3]->get_value());
         $this->assertEquals(3, $altResponses[4]->get_score());
         $this->assertEquals(['<img src="images/664892_p01_gi01.gif"/>', '<img src="images/664892_p01_gi02.gif"/>'], $altResponses[4]->get_value());
+    }
+
+    // TODO: Tin - Remove this or obsecure client data when done
+    public function testWithScenarioGivenByTinExactMatch()
+    {
+        $mapper = AppContainer::getApplicationContainer()->get('qtiv2_item_mapper');
+        list($item, $questions, $exceptions) = $mapper->parse($this->getFixtureFileContents('reallife/fromtin_exactmatch.xml'));
+
+        /** @var imageclozeassociation $question */
+        $question = $questions[0]->get_data();
+        $validation = $question->get_validation();
+
+        // Only 1 validation
+        $this->assertEquals(1, $validation->get_valid_response()->get_score());
+        $this->assertEquals([
+            null,
+            null,
+            '<img src="../images/FROERI727_Temperature_opt_c01_o_e111794163.png"/>',
+            null,
+            '<img src="../images/FROERI727_Temperature_opt_E01_o_7d0338b7ff.png"/>',
+        ], $validation->get_valid_response()->get_value());
+    }
+
+    // TODO: Tin - Remove this or obsecure client data when done
+    public function testWithScenarioGivenByTinMapResponse()
+    {
+        $mapper = AppContainer::getApplicationContainer()->get('qtiv2_item_mapper');
+        list($item, $questions, $exceptions) = $mapper->parse($this->getFixtureFileContents('reallife/fromtin_mapresponse.xml'));
+
+        /** @var imageclozeassociation $question */
+        $question = $questions[0]->get_data();
+        $validation = $question->get_validation();
+
+        // Got 1 main valid response
+        $this->assertEquals(2, $validation->get_valid_response()->get_score());
+        $this->assertEquals([
+            null,
+            null,
+            '<img src="../images/FROERI727_Temperature_opt_c01_o_e111794163.png"/>',
+            null,
+            '<img src="../images/FROERI727_Temperature_opt_E01_o_7d0338b7ff.png"/>',
+        ], $validation->get_valid_response()->get_value());
+
+        // Got 3 other alt responses
+        $this->assertCount(3, $validation->get_alt_responses());
+        // Alt response 1
+        $this->assertEquals([
+            null,
+            null,
+            '<img src="../images/FROERI727_Temperature_opt_c01_o_e111794163.png"/>',
+            null,
+            null,
+        ], $validation->get_alt_responses()[0]->get_value());
+        // Alt response 2
+        $this->assertEquals([
+            null,
+            null,
+            null,
+            null,
+            '<img src="../images/FROERI727_Temperature_opt_E01_o_7d0338b7ff.png"/>',
+        ], $validation->get_alt_responses()[1]->get_value());
+        // Alt response 3
+        $this->assertEquals([
+            null,
+            null,
+            null,
+            null,
+            null,
+        ], $validation->get_alt_responses()[2]->get_value());
     }
 }
