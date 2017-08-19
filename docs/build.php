10d1867a8c18f122f450538ed3f74d898330425d
Michael Sharman <sharmo@gmail.com>
2017-08-19 14:07:42 -0700

    Rebuilt docs

--- docs/build.php
+++ docs/build.php
@@ -5,10 +5,10 @@ require __DIR__ . '/../vendor/autoload.php';
 use LearnosityQti\AppContainer;
 
 // Generate entities
-// $generator = AppContainer::getApplicationContainer()->get('learnosity_entity_generator');
-// $generator->generateQuestionsClasses();
-// $generator->generateItemClasses();
-// $generator->generateActivityClasses();
+$generator = AppContainer::getApplicationContainer()->get('learnosity_entity_generator');
+$generator->generateQuestionsClasses();
+$generator->generateItemClasses();
+$generator->generateActivityClasses();
 
 // Generate documentation
 $generator = AppContainer::getApplicationContainer()->get('learnosity_documentation_generator');
