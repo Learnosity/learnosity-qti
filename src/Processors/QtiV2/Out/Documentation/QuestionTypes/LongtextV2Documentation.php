4024be46e0470d7975d164489ea0ba647f77675d
Michael Sharman <sharmo@gmail.com>
2017-08-24 11:07:32 +1000

    Added longtextv2 to docs

---
+++ src/Processors/QtiV2/Out/Documentation/QuestionTypes/LongtextV2Documentation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LearnosityQti\Processors\QtiV2\Out\Documentation\QuestionTypes;
+
+use LearnosityQti\Processors\QtiV2\Out\Documentation\LearnosityDoc;
+use LearnosityQti\Processors\QtiV2\Out\Documentation\QuestionTypeDocumentationInterface;
+
+class LongtextV2Documentation implements QuestionTypeDocumentationInterface
+{
+    public static function getDocumentation()
+    {
+        return [
+            'stimulus' => LearnosityDoc::support('This is mapped to `prompt`'),
+            'placeholder' => LearnosityDoc::support('This is mapped to the interaction `placeholder` attribute')
+        ];
+    }
+
+    public static function getIntroductionNotes()
+    {
+        return
+            "This question type is mapped to <extendedTextEntryInteraction> with `<strong>format</strong>` attribute as `xhtml`. " .
+            "Both the interaction attribute `minString` and `maxString` is mapped to 1." .
+            "Validation is not supported thus <responseProcessing> and <responseDeclaration> won't be populated.";
+    }
+}
