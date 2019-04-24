bac1127d122a59f1e69a3bef1e524187c77bd4b4
qainfotech <rakesh.gupta@qualitydsa.com>
2019-04-24 18:53:14 +0530

    test case updated/added

--- tests/Integration/Processors/QtiV2/Out/QuestionTypes/PlaintextMapperTest.php
+++ tests/Integration/Processors/QtiV2/Out/QuestionTypes/PlaintextMapperTest.php
@@ -11,22 +11,51 @@ class PlaintextMapperTest extends AbstractQuestionTypeTest
     public function testSimpleWithNoValidation()
     {
         $data = json_decode($this->getFixtureFileContents('learnosityjsons/item_plaintext.json'), true);
-        $assessmentItem = $this->convertToAssessmentItem($data);
+        $assessmentItemArray = $this->convertToAssessmentItem($data);
 
-        // Plaintext shall have no <responseDeclaration> and <responseProcessing>
-        $this->assertEquals(1, $assessmentItem->getResponseDeclarations()->count());
-        $this->assertNull($assessmentItem->getResponseProcessing());
 
-        // Has <extendedTextInteraction> as the first and only interaction
-        /** @var ExtendedTextInteraction $interaction */
-        $interaction = $assessmentItem->getComponentsByClassName('extendedTextInteraction', true)->getArrayCopy()[0];
-        $this->assertTrue($interaction instanceof ExtendedTextInteraction);
+        foreach($assessmentItemArray as $assessmentItem) {
+            $this->assertEquals(0, $assessmentItem->getResponseDeclarations()->count());
+            $this->assertNull($assessmentItem->getResponseProcessing());
 
-        // And its prompt is mapped correctly
-        $promptString = QtiMarshallerUtil::marshallCollection($interaction->getPrompt()->getComponents());
-        $this->assertEquals('<p>Write an essay</p>', $promptString);
 
-        // And it is a HTML text by default
-        $this->assertEquals(TextFormat::PLAIN, $interaction->getFormat());
+            // Has <extendedTextInteraction> as the first and only interaction
+            /** @var ExtendedTextInteraction $interaction */
+            $interaction = $assessmentItem->getComponentsByClassName('extendedTextInteraction', true)->getArrayCopy()[0];
+            $this->assertTrue($interaction instanceof ExtendedTextInteraction);
+
+            // And its prompt is mapped correctly
+            $promptString = QtiMarshallerUtil::marshallCollection($interaction->getPrompt()->getComponents());
+            $this->assertEquals('<p>Write an essay</p>', trim($promptString));
+
+            // And it is a HTML text by default
+            $this->assertEquals(TextFormat::PLAIN, $interaction->getFormat());
+        }
+    }
+    
+    public function testSimpleWithValidation()
+    {
+        $data = json_decode($this->getFixtureFileContents('learnosityjsons/item_plaintext_withvalidation.json'), true);
+        $assessmentItemArray = $this->convertToAssessmentItem($data);
+
+
+        foreach($assessmentItemArray as $assessmentItem) {
+            $this->assertEquals(1, $assessmentItem->getResponseDeclarations()->count());
+            $this->assertNotNull($assessmentItem->getResponseProcessing());
+            
+            // Has <extendedTextInteraction> as the first and only interaction
+            /** @var ExtendedTextInteraction $interaction */
+            $interaction = $assessmentItem->getComponentsByClassName('extendedTextInteraction', true)->getArrayCopy()[0];
+            $this->assertTrue($interaction instanceof ExtendedTextInteraction);
+            
+            // And its prompt is mapped correctly
+            $promptString = QtiMarshallerUtil::marshallCollection($interaction->getPrompt()->getComponents());
+            $this->assertEquals('Write an short essay on India -', trim($promptString));
+            $this->assertEquals('Please write somethig', $interaction->getPlaceholderText());
+            $this->assertEquals(1000, $interaction->getExpectedLength());
+            $this->assertEquals(1000, $interaction->getMaxStrings());
+            // And it is a HTML text by default
+            $this->assertEquals(TextFormat::PLAIN, $interaction->getFormat());
+        }
     }
 }
