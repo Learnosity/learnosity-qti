855d262be47ec7c14896d51dc16d268aeaef09f9
Ashish Kumar <ashish_kumar@qainfotech.com>
2019-05-07 16:57:31 +0530

    Fix issue where the stem of multi-part question are being stripped WBL-20

--- src/Processors/QtiV2/In/ItemBuilders/RegularItemBuilder.php
+++ src/Processors/QtiV2/In/ItemBuilders/RegularItemBuilder.php
@@ -44,6 +44,7 @@ class RegularItemBuilder extends AbstractItemBuilder
         }
 
         foreach ($interactionComponents as $component) {
+            
             /* @var $component Interaction */
             $questionReference = $this->itemReference . '_' . $component->getResponseIdentifier();
 
@@ -57,7 +58,8 @@ class RegularItemBuilder extends AbstractItemBuilder
                 [$component, $responseDeclaration, $responseProcessingTemplate, $outcomeDeclaration]
             );
             $question = $mapper->getQuestionType();
-
+            
+            
             $this->questions[$questionReference] = new Question($question->get_type(), $questionReference, $question);
             $questionsXmls[$questionReference] = [
                 'qtiClassName' => $component->getQtiClassName(),
@@ -67,10 +69,11 @@ class RegularItemBuilder extends AbstractItemBuilder
 
         // Build item's HTML content
         $extraContentHtml = new SimpleHtmlDom();
+        
         if (!$extraContentHtml->load(QtiMarshallerUtil::marshallCollection($itemBody->getComponents()), false)) {
             throw new \Exception('Issues with the content for itemBody, it might not be valid');
         }
-
+        
         // Extra stimulus for each question.
         // HACK: Process whole DOM structure per interaction.
         $dom = new DOMDocument();
@@ -78,9 +81,12 @@ class RegularItemBuilder extends AbstractItemBuilder
         // NOTE: Make sure we wrap in an <itemBody> so we get the correct DOM structure (and documentElement)
         $dom->loadHTML('<?xml version="1.0" encoding="UTF-8"><itemBody>'.$extraContentHtml->save().'</itemBody>', LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED);
         $xpath = new DOMXPath($dom);
-
+        
         $questionHtmlContents = [];
+        $contentList = '';
+        $previousContent = '';
         foreach ($questionsXmls as $questionReference => $interactionData) {
+            
             // Append this question span to our `item` content as it is
             $this->content .= '<span class="learnosity-response question-' . $questionReference . '"></span>';
 
@@ -89,86 +95,32 @@ class RegularItemBuilder extends AbstractItemBuilder
 
             $toQuery = '//'.strtolower($qtiClassName).'[@responseidentifier="'.$responseIdentifier.'"]';
             // Clean up interaction HTML content
-            foreach ($xpath->query('/itembody'.$toQuery) as $element) {
-                $elementRoot = $this->getElementHierarchyRoot($element, $dom->documentElement);
-
-                // Write out the hierarchy structure to a new DOM to get the content
-                $newDom = new DOMDocument();
-                $newDom->preserveWhitespace = false;
-                $newXPath = new DOMXPath($newDom);
-                $newDom->appendChild($newDom->importNode($elementRoot, true));
-                foreach ($newXPath->query($toQuery) as $newElement) {
-                    // HACK: Remove any trailing sibling nodes (as it won't look sensible with the elements gone).
-                    $siblings = [];
-                    $nextNode = $newElement;
-                    while (!is_null($nextNode = $nextNode->nextSibling)) {
-                        $siblings[] = $nextNode;
-                    }
-                    foreach ($siblings as $siblingNode) {
-                        $siblingNode->parentNode->removeChild($siblingNode);
-                    }
-
-                    // Remove the node itself from the stimulus content
-                    // $newElement->parentNode->removeChild($newElement);
-                    // HACK: Put a placeholder so we can pop prompts in place later on.
-                    $placeholderNode = $newDom->createTextNode($questionReference);
-                    $newElement->parentNode->replaceChild($placeholderNode, $newElement);
-
-                    // TODO: When looking at siblings and ancestor relatives to keep, we should only keep text nodes and wrapping content
-                    // HACK: Forcefully remove other interactions from this hierarchy
-                    $extraInteractions = $newXPath->query('//'.strtolower($qtiClassName).'[@responseidentifier!="'.$responseIdentifier.'"]');
-                    foreach ($extraInteractions as $interaction) {
-                        $interaction->parentNode->removeChild($interaction);
-                    }
+            
+            //fetch each interaction content to get the stimulus 
+            $appnodes = $xpath->query('/itembody'.$toQuery.'/preceding-sibling::*');
+            for($j=0;$j<$appnodes->length;$j++){
+                if($appnodes->item($j)->nodeName== strtolower($qtiClassName)){
+                    continue;
                 }
-                // Remove the whole hierarchy from the remaining itemBody content
-                // TODO: Check if removing interactions while iterating on DOMNodeList causes issues
-                $elementRoot->parentNode->removeChild($elementRoot);
-
-                if (!isset($questionHtmlContents[$questionReference])) {
-                    $questionHtmlContents[$questionReference] = '';
-                }
-                $questionHtmlContents[$questionReference] .= $newDom->saveHTML();
+                $contentList .= $dom->saveHTML($appnodes->item($j));
             }
-        }
-
-        // Remove the wrapping <itemBody> before saving
-        // HACK: Instead of replacing with a fragment, replace with a div;
-        // this is so we have a document element to remove the XML declaration with
-        /* @var DOMNode $fragment */
-        $fragment = $dom->createElement('div');
-        while ($dom->documentElement->childNodes->length > 0) {
-            $fragment->appendChild($dom->documentElement->childNodes->item(0));
-        }
-        $dom->documentElement->parentNode->replaceChild($fragment, $dom->documentElement);
-        $extraContent = '';
-        if ($fragment->hasChildNodes()) {
-            $extraContent = $dom->saveHTML($dom->documentElement);
-        }
-
-        // Inject item content into stimulus per question
-        foreach ($questionHtmlContents as $questionReference => $content) {
+            
+            //replace the previous interaction content from this question stimulus
+            $stimulus = '<div>';
+            $stimulus .= str_replace($previousContent, '', $contentList);
+            $stimulus .= '</div>';
+            
+            //store the previous interaction stimulus
+            $previousContent = $contentList; 
+            
+            // Inject item content into stimulus per question
             $existingStimulus = $this->questions[$questionReference]->get_data()->get_stimulus();
-            // HACK: Replace placeholders in item content with <prompt> stimulus, and inject the whole thing
-            $newStimulus = str_replace($questionReference, $existingStimulus, $content);
-            $this->questions[$questionReference]->get_data()->set_stimulus($newStimulus);
-
-            LogService::log('Extra <itemBody> content is prepended to question stimulus and please verify as this `might` break item content structure');
+            $stimulus_content = $stimulus . $existingStimulus;
+            $this->questions[$questionReference]->get_data()->set_stimulus($stimulus_content);
+            
         }
-
-        // Making assumption question always has stimulus `right`?
-        // So, prepend the extra content on the stimulus on the first question
-        if (!empty(trim($extraContent))) {
-            $firstQuestionReference = key($this->questions);
-            $existingStimulus = $this->questions[$firstQuestionReference]->get_data()->get_stimulus();
-            $newStimulus = $extraContent . $existingStimulus;
-            $this->questions[$firstQuestionReference]->get_data()->set_stimulus($newStimulus);
-
-            LogService::log('Extra <itemBody> content is prepended to question stimulus and please verify as this `might` break item content structure');
-        }
-
+        
         // TODO: Confirm that calling processRubricBlock after generating the item content won't break anything
-
         // Process <rubricBlock> elements
         // NOTE: This step needs to be done after questions are generated
         foreach ($rubricBlockComponents as $rubricBlock) {
