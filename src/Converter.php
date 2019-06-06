986a3c02cfab06118a912fcb51de40ea8d5251ae
qainfotech <rakesh.gupta@qualitydsa.com>
2019-06-06 19:21:44 +0530

    remove trailing white spaces

--- src/Converter.php
+++ src/Converter.php
@@ -27,9 +27,9 @@ use Symfony\Component\Finder\SplFileInfo;
 
 class Converter
 {
-
     const INPUT_FORMAT_QTIV2P1 = 'qtiv2p1';
     const OUTPUT_FORMAT_LRN_JSON = 'json';
+    
     const LEARNOSITY_DATA_ITEM = 'item';
     const LEARNOSITY_DATA_QUESTION = 'question';
     const LEARNOSITY_DATA_QUESTION_DATA = 'questiondata';
@@ -41,11 +41,11 @@ class Converter
         $learnosityImagesDirectory = $learnosityDirectory . '/Images';
         FileSystemUtil::createOrReplaceDir($learnosityImagesDirectory);
         $allImages = array_merge(
-            self::extractFiles('*.jpg', $imscpDirectory, $learnosityImagesDirectory), 
+            self::extractFiles('*.jpg', $imscpDirectory, $learnosityImagesDirectory),
             self::extractFiles('*.jpeg', $imscpDirectory, $learnosityImagesDirectory),
             self::extractFiles('*.gif', $imscpDirectory, $learnosityImagesDirectory)
         );
-        
+
         // Brute extract all the xml excepts imsmanifest.xml
         $itemReferences = [];
         $failedQtiXmlFilename = [];
@@ -108,11 +108,13 @@ class Converter
         $manifestMapper = AppContainer::getApplicationContainer()->get('imscp_manifest_mapper');
         /** @var ManifestWriter $manifestWriter */
         $manifestWriter = AppContainer::getApplicationContainer()->get('learnosity_manifest_writer');
-        
+
         try {
             LogService::flush();
+            
             $manifest = $manifestMapper->parse($xmlString);
             list($activities, $activitiesTags, $itemsTags) = $manifestWriter->convert($manifest, $rules);
+            
             $messages = LogService::flush();
             return [$activities, $activitiesTags, $itemsTags, $messages];
         } catch (Exception $e) {
@@ -124,7 +126,7 @@ class Converter
     {
         $testMapper = AppContainer::getApplicationContainer()->get('qtiv2_test_mapper');
         $activityWriter = AppContainer::getApplicationContainer()->get('learnosity_activity_writer');
-        
+
         try {
             /** @var TestMapper $testMapper */
             list($activity, $exceptions) = $testMapper->parse($xmlString, $validate);
@@ -138,6 +140,7 @@ class Converter
                 throw $e;
             }
         }
+        
         $activityData = $activityWriter->convert($activity);
         return [$activityData, $exceptions];
     }
@@ -147,6 +150,7 @@ class Converter
         $exceptions = null;
         $widgetWriter = AppContainer::getApplicationContainer()->get('learnosity_question_writer');
         $passageMapper = new SharedPassageMapper();
+        
         $widget = null;
         // Parse `em
         try {
@@ -164,7 +168,7 @@ class Converter
                 throw $e;
             }
         }
-        
+
         // Conversion to JSON
         $widgetData = [];
         if ($widget instanceof Question) {
@@ -178,7 +182,7 @@ class Converter
         $itemMapper = AppContainer::getApplicationContainer()->get('qtiv2_item_mapper');
         $itemWriter = AppContainer::getApplicationContainer()->get('learnosity_item_writer');
         $questionWriter = AppContainer::getApplicationContainer()->get('learnosity_question_writer');
-        
+
         // Parse `em
         try {
             $sourceDirectoryPath = null;
@@ -227,7 +231,7 @@ class Converter
         if ($rubricItem instanceof item) {
             $rubricItemData = $itemWriter->convert($rubricItem);
         }
-        
+
         $questionsData = [];
         if (is_array($questions)) {
             foreach ($questions as $question) {
@@ -242,7 +246,7 @@ class Converter
         }
         $layoutService = new \LearnosityQti\Services\ItemLayoutService();
         $itemData = $layoutService->migrateItem($itemData, array_merge($questionsData, $featuresData));
-        
+
         return [
             'item' => $itemData,
             'questions' => $questionsData,
@@ -255,22 +259,22 @@ class Converter
     public static function convertLearnosityToQtiItem(array $data)
     {
         $jsonType = self::guessLearnosityJsonDataType($data);
-        
+
         // Handle `item` which contains both a single item and one or more questions/features
         if ($jsonType === self::LEARNOSITY_DATA_ITEM) {
             list($xmlString, $messages) = self::convertLearnosityItem($data);
-        
+
         // Handle if just question
         } elseif ($jsonType === self::LEARNOSITY_DATA_QUESTION) {
             list($xmlString, $messages) = self::convertLearnosityQuestion($data);
-        
+
         // Handle if just question data
         } elseif ($jsonType === self::LEARNOSITY_DATA_QUESTION_DATA) {
             list($xmlString, $messages) = self::convertLearnosityQuestionData($data);
         } else {
             throw new \Exception('Unknown JSON format');
         }
-        
+
         // Validate them before proceeding by feeding it back
         try {
             $document = new XmlDocument();
@@ -296,7 +300,7 @@ class Converter
         $preprocessingService = new LearnosityToQtiPreProcessingService();
         $questionMapper = new QuestionMapper();
         $questionWriter = new QuestionWriter();
-        
+
         $question = $questionMapper->parseDataOnly($preprocessingService->processJson($questionDataJson));
         return $questionWriter->convert($question);
     }
@@ -311,7 +315,7 @@ class Converter
         $preprocessingService = new LearnosityToQtiPreProcessingService($questionsJson);
         $questionsJson = $preprocessingService->processJson($questionsJson);
         $itemJson = $preprocessingService->processJson($itemJson);
-        
+
         // Map those bad boys to Learnosity entities
         $itemMapper = new ItemMapper();
         $questionMapper = new QuestionMapper();
@@ -335,7 +339,7 @@ class Converter
         if ($data == null) {
             throw new MappingException('Invalid JSON');
         }
-        
+
         // Guess this JSON is an `item`
         if (!isset($data['type'])) {
             if (!isset($data['reference']) && !isset($data['content'])) {
@@ -343,7 +347,7 @@ class Converter
             }
             return self::LEARNOSITY_DATA_ITEM;
         }
-        
+
         // Guess this JSON is a `question`
         if (isset($data['data'])) {
             if (!isset($data['reference'])) {
