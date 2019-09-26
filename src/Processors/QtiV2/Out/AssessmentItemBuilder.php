34ca34d4dbfbfa8bd364e44cb1a9b93ad605de0e
pooja31chaudhary <49148553+pooja31chaudhary@users.noreply.github.com>
2019-09-26 20:30:09 +0530

    QtiV2/Out: Support converting feature widgets to QTI

--- src/Processors/QtiV2/Out/AssessmentItemBuilder.php
+++ src/Processors/QtiV2/Out/AssessmentItemBuilder.php
@@ -3,10 +3,8 @@ namespace LearnosityQti\Processors\QtiV2\Out;
 
 use LearnosityQti\Entities\Question;
 use LearnosityQti\Exceptions\MappingException;
-use LearnosityQti\Utils\StringUtil;
 use qtism\common\enums\BaseType;
 use qtism\common\enums\Cardinality;
-use qtism\common\utils\Format;
 use qtism\data\AssessmentItem;
 use qtism\data\content\FlowStaticCollection;
 use qtism\data\content\ModalFeedback;
@@ -44,7 +42,7 @@ class AssessmentItemBuilder
         $assessmentItem = new AssessmentItem($itemIdentifier, $itemIdentifier, false);
         $assessmentItem->setLabel($itemLabel);
         $assessmentItem->setToolName('Learnosity');
-
+        
         // Store interactions on this array to later be placed on <itemBody>
         $interactions = [];
         $responseDeclarationCollection = new ResponseDeclarationCollection();
