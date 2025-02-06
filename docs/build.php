4412c05d0855c3c42f30e9b24f35c38a017e8989
Michael Sharman <sharmo@gmail.com>
2025-02-06 21:19:43 +1100

    Added twig extensions and fixed issue with auto-generated entities.

--- docs/build.php
+++ docs/build.php
@@ -4,6 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use LearnosityQti\AppContainer;
 
+die('This script is disabled for now. Major issues updating the schema.');
+
 // Generate entities
 $generator = AppContainer::getApplicationContainer()->get('learnosity_entity_generator');
 $generator->generateQuestionsClasses();
@@ -11,5 +13,5 @@ $generator->generateItemClasses();
 $generator->generateActivityClasses();
 
 // Generate documentation
-$generator = AppContainer::getApplicationContainer()->get('learnosity_documentation_generator');
-$generator->generateDocumentation();
+// $generator = AppContainer::getApplicationContainer()->get('learnosity_documentation_generator');
+// $generator->generateDocumentation();
